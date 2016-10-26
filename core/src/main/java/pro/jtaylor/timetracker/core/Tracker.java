e481cabc2908d34aca02422c247da686cb53ff96
subashpothini <subash.pothini@gmail.com>
2016-10-25 22:55:21 -0500

    make some improvements

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -24,6 +24,9 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
+        boolean valid = false;
+        if(valid == true){
+        }
         return entries.get(index);
     }
 }
